d9884ed9fcbbf8db33b4176a3a4727b51c7f9f49
LuckyForce <adrian.schauer@aon.at>
2022-04-29 13:19:52 +0200

    Init

---
+++ post.php
@@ -0,0 +1,30 @@
+<?php
+namespace EmailAPI;
+
+class Post {
+    public function __construct() {
+        //
+    }
+
+    public function sendMail() {
+        //Send Mail
+    }
+
+    public function setHTMLFooter() {
+        return $this->footer;
+    }
+
+    public function setHTMLHeader() {
+        return $this->header;
+    }
+
+    public function setFooter() {
+        return $this->footer;
+    }
+
+    public function setHeader() {
+        return $this->header;
+    }
+}
+
+
